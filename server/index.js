73db4212be0fd939c5799d807537158c799e4ffb
You Song Hou <ysongweb3@gmail.com>
2025-01-18 01:30:33 -0500

    feat: add API to fetch video transcripts/captions using youtube-transcript-api

--- server/index.js
+++ server/index.js
@@ -3,12 +3,25 @@ require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const YoutubeTranscript = require("youtube-transcript-api");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
+
+app.get('/transcript/:videoId', async (req, res) => {
+  try {
+    const videoId = req.params.videoId;
+    const transcript = await YoutubeTranscript.getTranscript(videoId);
+
+    res.json({ transcript: transcript });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.get('/', (req, res) => res.send('It Work'));
 

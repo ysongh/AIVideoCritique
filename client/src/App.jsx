6aff45ccd63fdb06ceaf833c67c790ce79bf9b04
You Song Hou <ysongweb3@gmail.com>
2025-01-27 02:00:54 -0500

    feat: install 'react-router-dom'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
+import { HashRouter, Route, Routes } from 'react-router-dom';
+
 import FeedbackForm from "./pages/FeedbackForm";
 
 function App() {
-
   return (
-    <>
-      <FeedbackForm />
-    </>
+    <HashRouter>
+      <Routes>
+        <Route
+          path="/test"
+          element={
+            <>
+              <h1>Test</h1>
+            </>} />
+        <Route
+          path="/"
+          element={<FeedbackForm />} />
+      </Routes>
+    </HashRouter>
   )
 }
 
